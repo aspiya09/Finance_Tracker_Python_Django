c672de1db914fb6cc363cf3ac73b6234fae2371e
KaziShahHamza <kazishahhamza@outlook.com>
2025-07-30 14:04:44 +0600

    Add edit, delete button

--- tracker/urls.py
+++ tracker/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('edit/<int:pk>/', views.edit_transaction, name='edit_transaction'),
+    path('delete/<int:pk>/', views.delete_transaction, name='delete_transaction'),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Transaction
 from .forms import TransactionForm
+from django.shortcuts import get_object_or_404
+
 
 def index(request):
     transactions = Transaction.objects.order_by('-date')
@@ -21,3 +23,32 @@ def index(request):
         'expense': expense,
         'balance': balance
     })
+
+def edit_transaction(request, pk):
+    transaction = get_object_or_404(Transaction, pk=pk)
+    form = TransactionForm(request.POST or None, instance=transaction)
+
+    if request.method == 'POST' and form.is_valid():
+        form.save()
+        return redirect('home')
+
+    transactions = Transaction.objects.order_by('-date')
+    income = sum(t.amount for t in transactions if t.type == 'income')
+    expense = sum(t.amount for t in transactions if t.type == 'expense')
+    balance = income - expense
+
+    return render(request, 'tracker/index.html', {
+        'form': form,
+        'transactions': transactions,
+        'income': income,
+        'expense': expense,
+        'balance': balance,
+        'edit_mode': True,
+        'edit_id': pk,
+    })
+
+
+def delete_transaction(request, pk):
+    transaction = get_object_or_404(Transaction, pk=pk)
+    transaction.delete()
+    return redirect('home')
