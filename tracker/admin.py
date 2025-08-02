88f4813fefeb9e3cf2060884bf0a6206f3701b9d
KaziShahHamza <kazishahhamza@outlook.com>
2025-08-02 10:47:41 +0600

    Add django admin

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Transaction
 
-# Register your models here.
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'amount', 'type', 'date')
+
+admin.site.register(Transaction, TransactionAdmin)
